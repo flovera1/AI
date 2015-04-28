b0ee117457bd128d24508ae850dd1a8fd959678a
FernandoLovera <flovera1@gmail.com>
2015-04-28 12:06:57 -0430

    adding the plot data

--- Descendent gradient /gradient.cpp
+++ Descendent gradient /gradient.cpp
@@ -1,13 +1,27 @@
+/**
+* @description: this code is used to demostrate the batch descendent gradient. 
+*	  		 	What happens here is that we update the thetas until find a value that
+* 		  	 	match (or gets really close) to the expected value(values in the hypothesis).
+* 	  	  	  	the simplicity of the code talks by itself. To view the data, the files ".data"
+* 	  	  	 	were created. and then you can use it with gnuplot. Enjoy :)	
+*	  	 	 	
+* @author: Fernando Lovera flovera1@gmail.com
+* @Date: Tue 28 Apr 2015
+*/
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
 #include <vector>
+#include <stdio.h>
+// basic file operations
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 double a, b;
 double theta0 = 0.0, theta1 = 0.0;
 double myrand() {
-  return double(rand()) / RAND_MAX;
+  return rand() % 10;
 }
 double f(double x) {
   double y = a * x + b;
@@ -32,9 +46,14 @@ int main() {
   int N = 10000;
   vector<double> xs(N);
   vector<double> ys(N);
+  ofstream myfile0;
+  ofstream myfile1;
+  myfile0.open ("hypothesis.dat");
+  myfile1.open ("gradientDescending.dat");
   for (int i = 0; i < N; ++i) {
     xs[i] = myrand();
     ys[i] = f(xs[i]);
+  	myfile0 << xs[i] << " " << ys[i] << "\n";
   }
   double learninRate = 0.010;
   double d0, d1;
@@ -48,6 +67,9 @@ int main() {
     theta0 -= (learninRate/N) * d0;
     theta1 -= (learninRate/N) * d1;
     printf("theta1: %lf, theta0: %lf\n", theta1, theta0);
+    myfile1 << theta0 << " " << theta1 << "\n";
   }
+  myfile0.close();
+  myfile1.close();
   return 0;
 }

45d01035704aa4cb8679fbaf37ddaf84b4f1345c
flovera1@gmail.com <flovera1@gmail.com>
2015-05-29 13:02:43 -0430

    beginning sklearn

---
+++ SKLearn_Python/svm.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn import svm
+
+digits = datasets.load_digits()
+"""
+print(digits.data)
+print(digits.target)
+print(digits.images[0])
+"""
+clf  = svm.SVC(gamma = 0.001, C = 100)
+#print(len(digits.data))
+#training the data
+x, y = digits.data[:-10], digits.target[:-10] #storing all the answers
+clf.fit(x, y)
+print('Prediction:', clf.predict(digits.data[-6]))
+#show the image of the digit that we are printing
+plt.imshow(digits.images[-1], cmap = plt.cm.gray_r, interpolation = "nearest")
+plt.show()
